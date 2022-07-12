7ee039ad3e9b58f2756a46bed575a3efa404dfb7
seanjungmsba <seanjeong1996@gmail.com>
2022-07-12 17:05:51 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        // your code here
+public class Logger {
+    /*
+     * A private static member variable of the type Logger
+     * that will be the only instance of Logger ever created
+     */
+    private static Logger logger = null;
+    private int currentLine = 0;
+
+    /*
+     * A private constructor, so that the singleton class can
+     * never be created with the new keyword, like regular classes can
+     */
+    private Logger() {}
+
+    /*
+     * A static, public method, let's call it getInstance(),
+     * that either returns the existing single instance of the class
+     * or creates the single instance if it hasn't already been created and then returns it
+     */
+    public static Logger getInstance() {
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger;
     }
+
+    public void log(String msg) {
+        currentLine++;
+        System.out.println(currentLine + "::" + msg);
+    }
+
+    public int getCurrentLine() {
+        return currentLine;
+    }
+
 }
